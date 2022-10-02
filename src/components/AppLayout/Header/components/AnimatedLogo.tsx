a0041a530ffab2b355f85ccf5111ef5a86bc2355
Rami Husami <husamirami@gmail.com>
2022-10-02 18:12:35 +0300

    fuse safe presonalization

--- src/components/AppLayout/Header/components/AnimatedLogo.tsx
+++ src/components/AppLayout/Header/components/AnimatedLogo.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, ReactElement } from 'react'
-import SafeLogo from '../assets/transition-logo.gif'
+import SafeLogo from '../assets/safe-logo.svg'
 
 const useInterval = (delay: number): number => {
   const [count, setCount] = useState<number>(0)
